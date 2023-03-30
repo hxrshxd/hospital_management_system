fa1a71f2df4131a4a2585683109814578794640e
Harshad Rathod <rharshad26@gmail.com>
2023-03-30 17:30:19 +0530

    added doctor class

---
+++ src/main/java/com/example/hospital_management_system/doctor/Doctor.java
@@ -0,0 +1,37 @@
+package com.example.hospital_management_system.doctor;
+
+public class Doctor {
+    private String name;
+    private int age;
+    private String speciality;
+
+    public Doctor(String name, int age, String speciality) {
+        this.name = name;
+        this.age = age;
+        this.speciality = speciality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
